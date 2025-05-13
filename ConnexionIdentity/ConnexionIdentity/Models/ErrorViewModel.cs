e7903cc96ed0f61deed2aed7f75c2e0d346911bb
Jaafar Hadeg <Jaafar.Hadeg@emsi-edu.ma>
2025-05-13 15:33:46 +0100

    sdfgh

---
+++ ConnexionIdentity/ConnexionIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConnexionIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ projetpush/projetpush/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projetpush.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
